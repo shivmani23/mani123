0761cd082f13ad33a4b6a83db83529ab125153bb
ANISHU <ANISHU@ANISHU>
2024-10-05 13:35:04 +0530

    added scripts and lead package

--- DummyModule/src/test/java/OrganizationTest/toCreateOrganizationTest.java
+++ DummyModule/src/test/java/OrganizationTest/toCreateOrganizationTest.java
@@ -2,7 +2,9 @@ package OrganizationTest;
 
 public class toCreateOrganizationTest {
 	public static void main(String[] args) {
+		System.out.println("write the organization name in the mandatory field");
 		System.out.println("organization test has been completed");
+		System.out.println("Organization test has been validated successfully");
 	}
 
 }
--- DummyModule/src/test/java/contactTest/toCeateContactTest.java
+++ DummyModule/src/test/java/contactTest/toCeateContactTest.java
@@ -2,6 +2,8 @@ package contactTest;
 
 public class toCeateContactTest {
 	public static void main(String[] args) {
+		System.out.println("Aniket create some  test scripts in this class");
+		System.out.println("I have validated the test scripts also");
 		System.out.println("this test cases is validated");
 	}
 
